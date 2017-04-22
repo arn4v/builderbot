bb50c23703a2cdc669e5291f5a6ab534d71003ef
Harsh Shandilya <msfjarvis@gmail.com>
2017-04-22 17:47:59 +0530

    Add restart command

--- build.py
+++ build.py
@@ -6,6 +6,9 @@ import logging
 import os
 import subprocess
 
+import time
+
+import sys
 from telegram import ChatAction
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
@@ -37,24 +40,36 @@ def build(bot, update):
             document=open(filename, "rb"),
             chat_id=update.message.chat_id)
 
+
 def upload(bot, update):
     if update.message.from_user.id in sudo_users:
         bot.sendChatAction(chat_id=update.message.chat_id,
-                            action=ChatAction.TYPING)
+                           action=ChatAction.TYPING)
         bot.sendMessage(chat_id=update.message.chat_id,
                         text="Uploading to the chat")
         os.chdir(path + "/out")
         filename = os.listdir(".")[0]
         bot.sendDocument(
-            document=open(filename,rb),
+            document=open(filename, "rb"),
             chat_id=update.message.chat_id)
+
+
+def restart(bot, update):
+    if update.message.from_user.id in sudo_users:
+        bot.sendMessage(update.message.chat_id, "Bot is restarting...")
+        time.sleep(0.2)
+        os.execl(sys.executable, sys.executable, *sys.argv)
+    else:
+        bot.sendMessage(update.message.chat_id, "Ummm, nope.")
 
 
 build_handler = CommandHandler('build', build)
 upload_handler = CommandHandler('upload', upload)
+restart_handler = CommandHandler('restart', restart)
 
 dispatcher.add_handler(build_handler)
 dispatcher.add_handler(upload_handler)
+dispatcher.add_handler(restart_handler)
 
 updater.start_polling()
 updater.idle()
